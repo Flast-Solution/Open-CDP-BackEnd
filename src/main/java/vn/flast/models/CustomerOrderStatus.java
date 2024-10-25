2ae436e329895859d2b59e565765f95b423e23a8
HuuLong <long.huu.100@gmail.com>
2024-10-25 10:03:38 +0700

    order status

---
+++ src/main/java/vn/flast/models/CustomerOrderStatus.java
@@ -0,0 +1,27 @@
+package vn.flast.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "customer_order_status")
+@Entity
+@Getter @Setter
+public class CustomerOrderStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "del_flag")
+    private Integer delFlag;
+}
